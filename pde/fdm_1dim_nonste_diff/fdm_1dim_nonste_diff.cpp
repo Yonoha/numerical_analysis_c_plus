2092ca06632ac40d0c0c06e32e3e2576abe8bdd5
Yonoha <75283304+Yonoha@users.noreply.github.com>
2022-07-13 13:45:00 +0900

    Update fdm_1dim_nonste_diff.cpp

--- pde/fdm_1dim_nonste_diff/fdm_1dim_nonste_diff.cpp
+++ pde/fdm_1dim_nonste_diff/fdm_1dim_nonste_diff.cpp
@@ -1,137 +1,157 @@
-// 1-dim nonsteady diffusion equation by fdm
+// 1dim non-steady diffusion equation by fdm
+// use center differential method
 
 #include <cmath>    // for std::sin
-#include <fstream>  // for std::ofstream 
-#include <iostream> // for std::cerr, std::endl
-#include <vector>   // for std::vector
+#include <cstdio>   // for std::fclose, std::fopen, std::fprintf
+#include <iostream> // for std::endl
+#include <memory>   // for std::unique_ptr
+#include <vector>   // fos std::vector
 #include <boost/assert.hpp>                   // for BOOST_ASSERT
+#include <boost/format.hpp>                   // for boost::format
 #include <boost/math/constants/constants.hpp> // for boost::math::constants::pi
 
 namespace{
     using myvec = std::vector<double>;
 
-    static auto constexpr TEND = 10.0;
-    static auto constexpr TLOOP = 10000;
-    static auto constexpr DT = TEND / TLOOP;
-    static auto constexpr TREP = TLOOP / 1000;
-
-    static auto constexpr MESH = 100;
-    static auto constexpr XRANGE = 1.0;
-    static auto constexpr DX = XRANGE / MESH;
-
     // boundary condition
-    static auto constexpr BOUND_LEFT = 0.0;
-    static auto constexpr BOUND_RIGHT = 0.0;
-    
-    // coefficient
-    static auto constexpr DIFF = 0.01;
-    static auto constexpr KAP = DIFF * DT / (DX * DX);
-    
-    // result output file
-    bool output_result(std::vector<myvec> const &uxt);
+    enum class boundary_conditon_type{
+        DIRICLET = 0,
+        LEFT_NEUMANN = 1,
+        RIGHT_NEUMANN = 2,
+        PERIODIC = 3,
+    };
 
-    // discretization of x
+    static auto constexpr BCT = boundary_conditon_type::PERIODIC;
+
+    static auto constexpr D0 = 0.0; // left Diriclet
+    static auto constexpr D1 = 0.0; // right Diriclet
+    static auto constexpr N0 = 0.0; // left Neumann
+    static auto constexpr N1 = 0.0; // right Neumann
+
+    static auto constexpr TEND = 1.0;
+    static auto constexpr TLOOP = 10000;
+    static auto constexpr TREP = 100;
+    static auto constexpr DT = TEND / TLOOP;
+
+    static auto constexpr XRANGE = 1.0;
+    static auto constexpr MESH = 100;
+    static auto constexpr NODE = MESH + 1;
+    static auto constexpr DX = XRANGE / MESH; 
+
+    // coefficient
+    static auto constexpr DIFF = 0.01; // diffusion number
+    static auto constexpr KAP = DIFF * DT / (DX * DX);
+
+    bool result_output(int const t, myvec const &u0);
+
     myvec discrete();
 
-    // initial condition u(x, 0)
     myvec initial(myvec const &x);
 
-    // center differential method
-    std::vector<myvec> differencial_method(myvec const &u0);
+    void center_diffe(myvec &u0);
 }
 
 int main(){
     // KAP > 0.5のとき強制終了 
     // Neumannの安定性解析より, KAP <= 0.5でないと正しい結果が得られない
-    BOOST_ASSERT(KAP <= 0.5);
+    BOOST_ASSERT(KAP <= 0.5); 
 
     auto x = discrete();
 
     auto u0 = initial(x);
 
-    auto uxt = differencial_method(u0);
-
-    output_result(uxt);
-
-    if (!output_result(uxt)){
-        std::cerr << "error! output file not open" << std::endl;
+    if (!result_output(0, u0)){
+		std::cerr << "output file not open" << std::endl;
         return -1;
+    }
+
+    for (auto t = 0; t <= TLOOP; t++){
+        center_diffe(u0);
+
+        if(!(t % (TLOOP / TREP))){
+            if (!result_output(t, u0)){
+		        std::cerr << "output file not open" << std::endl;
+                return -1;
+            }
+        }
     }
 
     return 0;
 }
 
 namespace{
-    bool output_result(std::vector<myvec> const &uxt){
-        std::vector<myvec> u(uxt.size(), (myvec(TREP + 1)));
-        
-        // output only TREP + 1 (TLOOP = 0, 1000, ... , 10000)
-        for (auto i = 0; i <= TREP; i++){
-            for (auto j = 0; j <= MESH; j++){
-                u[j][i] = uxt[j][1000 * i];
-            }
-        }
+	bool result_output(int const t, myvec const &u0){
+		auto const filename = boost::format("data_fdm_1dim_nonste_diff_%d.txt") % (static_cast<int>(t / (TLOOP / TREP)));
 
-        std::ofstream ofs;
-        ofs.open("data_center_diffe_diff.txt");
+		std::unique_ptr<FILE, decltype(&std::fclose)> fp(std::fopen(filename.str().c_str(), "w"), std::fclose);
 
-        if (!ofs){
-            return false;
-        }
+		if (!fp){
+			return false;
+		}
 
-        for (auto j = 0; j <= MESH; j++){
-            ofs << static_cast<double>(j) * DX << " ";
-            
-            for (auto i = 0; i <= TREP; i++){
-                ofs << u[j][i] << " ";
-            }
-            ofs << std::endl;
-        }
+		for (auto i = 0; i < NODE; i++){
+			std::fprintf(fp.get(), "%.2f %.7f\n", static_cast<double>(i) * DX, u0[i]);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
     myvec discrete(){
-        myvec x(MESH + 1, 0.0);
+        myvec x(NODE, 0.0);
 
         // discrization of xrange
-        for (int i = 0; i <= MESH; i++){
-            x[i] = i * DX;
+        for (auto i = 0; i < NODE; i++){
+            x[i] = static_cast<double>(i) * DX;
         }
 
         return x;
     }
 
     myvec initial(myvec const &x){
-        myvec u0(MESH + 1, 0.0);
+        using namespace boost::math::constants;
+        myvec u0(NODE, 0.0);
 
-        // initial condition
-        for (int i = 0; i<= MESH; i++){
-            u0[i] = std::sin(boost::math::constants::pi<double>() * x[i]);
+        // initial condition 
+        for (auto i = 0; i < NODE; i++){
+            u0[i] = std::sin(pi<double>() * x[i]);
         }
 
         return u0;
     }
 
-    std::vector<myvec> differencial_method(myvec const &u0){
-        std::vector<myvec> uxt(MESH + 1, (myvec(TLOOP + 1)));
+    void center_diffe(myvec &u0){
+        myvec u(NODE, 0.0);
 
-        // initial condition
-        for (auto j = 0; j <= MESH; j++){
-            uxt[j][0] = u0[j];
+        switch (BCT){
+            case boundary_conditon_type::DIRICLET:
+                u[0] = D0;
+                u[NODE - 1] = D1;
+                break;
+
+            case boundary_conditon_type::LEFT_NEUMANN:
+                u[0] = u0[0] + KAP * (u0[1] - u0[0] - DX * N0);
+                u[NODE - 1] = D1;
+                break;
+
+            case boundary_conditon_type::RIGHT_NEUMANN:
+                u[0] = D0;
+                u[NODE - 1] = u0[NODE - 1] + KAP * (DX * N1 - u0[NODE - 1] + u0[NODE - 2]);
+                break;
+
+            case boundary_conditon_type::PERIODIC:
+                u[0] = u0[0] + KAP * (u0[1] - 2.0 * u0[0] + u0[NODE - 1]);
+                // u[NODE - 1] = u[0];
+                u[NODE - 1] = u0[0] + KAP * (u0[0] - 2.0 * u0[NODE - 1] + u0[NODE - 2]);
+                break;
         }
 
-        for (auto i = 0; i < TLOOP; i++){
-            // boundary condition
-            uxt[0][i + 1] = BOUND_LEFT;
-            uxt[MESH][i + 1] = BOUND_RIGHT;
-
-            // center differential method
-            for (auto j = 1; j < MESH; j++){
-                uxt[j][i + 1] = uxt[j][i] + KAP * (uxt[j + 1][i] - 2.0 * uxt[j][i] + uxt[j - 1][i]);
-            }
+        for (auto j = 1; j < NODE - 1; j++){
+            u[j] = u0[j] + KAP * (u0[j + 1] - 2.0 * u0[j] + u0[j - 1]);
         }
 
-        return uxt;
+        // update of u
+        for (auto j = 0; j < NODE; j++){
+            u0[j] = u[j];
+        }
     }
 }
